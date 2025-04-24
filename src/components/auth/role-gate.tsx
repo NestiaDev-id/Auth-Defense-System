23a4eda47bd4782226b947ffbe151546145347a4
NestiaDev-id <yohanesdevano90@gmail.com>
2025-04-25 01:34:17 +0700

    update structure folder

---
+++ src/components/auth/role-gate.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { UserRole } from "@prisma/client";
+
+import { useCurrentRole } from "@/src/hooks/use-current-role";
+import { FormError } from "@/src/components/form-error";
+
+interface RoleGateProps {
+  children: React.ReactNode;
+  allowedRole: UserRole;
+}
+
+export const RoleGate = ({ children, allowedRole }: RoleGateProps) => {
+  const role = useCurrentRole();
+
+  if (role !== allowedRole) {
+    return (
+      <FormError message="You do not have permission to view this content!" />
+    );
+  }
+
+  return <>{children}</>;
+};
